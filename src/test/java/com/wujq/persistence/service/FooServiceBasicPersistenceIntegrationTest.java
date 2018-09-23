9ad2980ed3de0a48fa7a925c997b0dbcc519bf92
wujek-oczko <wujek-oczko@users.noreply.github.com>
2018-09-23 15:46:15 +0200

    Unnecessary dependencies removed

--- src/test/java/com/wujq/persistence/service/FooServiceBasicPersistenceIntegrationTest.java
+++ src/test/java/com/wujq/persistence/service/FooServiceBasicPersistenceIntegrationTest.java
@@ -2,10 +2,6 @@ package com.wujq.persistence.service;
 
 import com.wujq.persistence.model.Foo;
 import com.wujq.spring.PersistenceConfig;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PersistenceConfig.class}, loader = AnnotationConfigContextLoader.class)
@@ -31,7 +27,7 @@ public class FooServiceBasicPersistenceIntegrationTest {
 
     @Test
     public final void whenEntityIsCreated_thenNoExceptions() {
-        fooService.create(new Foo(randomAlphabetic(6)));
+        fooService.create(new Foo("Foo_persisted" + UUID.randomUUID().toString()));
     }
 
 }
